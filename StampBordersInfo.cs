994fc3c67b62a1ac64143d8f883684813efb913c
dmytri4 <dmytri4@gmail.com>
2016-11-19 13:26:35 +0200

    Add StampBorderInfo class

---
+++ StampBordersInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace RStamp
+{
+	[Serializable()]
+	public class StampBordersInfo
+	{
+		public double OuterBorderFirstThickness { get; set;}
+		public double OuterBorderSecondThickness { get; set; }
+		public double OuterBorderGap { get; set; }
+		public double InnerBorderThickness { get; set; }
+
+		//Default constructor
+		public StampBordersInfo()
+		{
+			OuterBorderFirstThickness = 0.4;
+			OuterBorderSecondThickness = 0.2;
+			OuterBorderGap = 0.2;
+			InnerBorderThickness = 0.2;
+		}
+	}
+}
