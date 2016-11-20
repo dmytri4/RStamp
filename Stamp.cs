235bfc0ba3a95016f3417674b66eb379775a3662
dmytri4 <dmytri4@gmail.com>
2016-11-20 14:02:27 +0200

    Serialization added

--- Stamp.cs
+++ Stamp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -7,15 +8,35 @@ namespace RStamp
 	[Serializable()]
 	public class Stamp
 	{
-		public double Diameter { get; set;}
+		public double Diameter { get; set; }
 		public StampBordersInfo Border;
 		public StampStrings Strings;
 
+		private string Base64EncodedCenterImg;
+
+		//Default Ctor
 		public Stamp()
 		{
 			Diameter = 50; //Rs=50mm
-			Border   = new StampBordersInfo();
-			Strings  = new StampStrings();
+			Border = new StampBordersInfo();
+			Strings = new StampStrings();
+		}
+
+		//Save Stamp object into xml file
+		public void SerializeToXml(string path)
+		{
+			XmlSerializer ser = new XmlSerializer(typeof(Stamp)); 	//Serializer component creation
+			TextWriter writer = new StreamWriter(path);				//Place to save xml file
+			ser.Serialize(writer, this);							//Serialization
+			writer.Close();											//Close writing stream after the end of serialization
+		}
+
+		//Load Stamp object from xml file
+		public static Stamp DeserializeFromXml(string path)
+		{
+			XmlSerializer ser = new XmlSerializer(typeof(Stamp));
+			TextReader reader = new StreamReader(path);
+			return (Stamp)ser.Deserialize(reader);
 		}
 	}
 }
