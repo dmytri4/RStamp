be9fa06c05a80477d101a000286b237edc2b2a7c
dmytri4 <dmytri4@gmail.com>
2016-11-20 14:23:09 +0200

    Added new params into StampImagesParams and StampImages classes

--- StampImages.cs
+++ StampImages.cs
@@ -5,13 +5,18 @@ namespace RStamp
 {
 	public class StampImages
 	{
-		public List<string> ImagesList { get; set;}
+		public List<string> ImagesList { get; set;} //base64 encoded images list
 		public List<StampImagesParams> Parameters { get; set;}
 
 		public StampImages()
 		{
 			ImagesList = new List<string>();
 			Parameters = new List<StampImagesParams>();
+		}
+
+		public void LoadImage(string path)
+		{
+			
 		}
 	}
 }
--- StampImagesParams.cs
+++ StampImagesParams.cs
@@ -3,6 +3,11 @@ namespace RStamp
 {
 	public class StampImagesParams
 	{
+		public int PixelWidth { get; set;}
+		public int PixelHeight { get; set;}
+		public double WidthMm { get; set;}
+		public double HeightMm { get; set;}
+
 		public StampImagesParams()
 		{
 		}
